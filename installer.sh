42a9b04575e8019ef38359a327964967fa77c5c3
JustGuus <justusdegroot@icloud.com>
2016-12-04 17:58:37 +0100

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+echo "\033[33;31mB  \033[33;32mR  \033[33;33mO  \033[33;36mK  \033[33;34mE  \033[33;35mN"
+echo "\033[33;31mDeveloper: \033[33;30mNoobGuus - http://github.com/NoobGuus"
+echo ""
+echo "=========================================================================="
+echo ""
+
+APPLICATIONS="apache2 tightvncserver avahi-daemon aircrack-ng"
+
+echo "LETS BEGIN!"
+sleep 2.5
+
+
+apt-get update
+sleep 1.5
+apt-get upgrade
+sleep 1.5
+apt-get install $APPLICATIONS
+sleep 1.5
+service apache2 start
+sleep 1.5
+service apache2 stop
+sleep 1.5
+sudo cp /home/pi/Broken-Project/index.html /var/www/html/index.html
+
+echo "LETS REBOOT TO BE SURE THAT EVERYTHING IS GOOD INSTALLED!"
+echo "Just type < sudo reboot now >"
